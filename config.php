ad1f45ff1a97cc6533be718f53b0e77774785cd5
Jordan <14931886+nykez@users.noreply.github.com>
2020-05-29 14:56:32 -0400

    Update config.php

--- config.php
+++ config.php
@@ -6,5 +6,6 @@ return [
     'github' => 'https://github.com/nykez',
     'twitter' => 'https://wakatime.com/@Nykez',
     'gmodstore' => 'https://www.gmodstore.com/users/76561198011844757',
+    'steam' => 'https://steamcommunity.com/id/Jordanlol633',
     'collections' => [],
 ];
