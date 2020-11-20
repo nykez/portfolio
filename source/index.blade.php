651f797d7e731f323dbd47074f5cbda4e640ff3c
Jordan <nykezgaming@live.com>
2020-11-20 00:32:19 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -20,21 +20,17 @@
                     <i class="fas fa-plug"></i>
                     Wakatime
                 </a>
-                <a href="{{ $page->steam }}" target="_blank" class="inline-block rounded-full bg-gray-600 uppercase px-2 py-1 text-xs font-bold text-red-100 font-bold">
-                    <i class="fa fa-steam"></i>
-                    Steam
-                </a>
-                   <a href="{{ $page->gmodstore }}" target="_blank" class="inline-block rounded-full bg-blue-700 uppercase px-2 py-1 text-xs font-bold text-red-100 font-bold">
-                    <i class="far fa-gamepad fa-fw"></i>
+                 <a href="{{ $page->gmodstore }}" target="_blank" class="inline-block rounded-full bg-blue-700 uppercase px-2 py-1 text-xs font-bold text-red-100 font-bold">
+                    <i class="fas fa-gamepad fa-fw"></i>
                     Gmodstore
                 </a>
 
                 <a href="mailto:nykezgaming@live.com" target="_blank" class="inline-block rounded-full bg-blue-700 uppercase px-2 py-1 text-xs font-bold text-red-100 font-bold">
-                    <i class="far fa-mailbox fa-fw"></i>
+                    <i class="fas fa-mailbox fa-fw"></i>
                     Email
                 </a>
                  <a href="https://www.linkedin.com/in/jcavins/" target="_blank" class="inline-block rounded-full bg-blue-700 uppercase px-2 py-1 text-xs font-bold text-red-100 font-bold">
-                    <i class="far fa-linkedin"></i>
+                    <i class="fab fa-linkedin-in"></i>
                     LinkedIn
                 </a>
             </div>
